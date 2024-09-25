91c98ef2f6ba89e3faed562e14349a1d4bf9f95f
vickyaltitude <vignvick3005>
2024-09-25 09:36:46 +0530

    learnt request response and page redirection

--- app.js
+++ app.js
@@ -1,6 +1,37 @@
 const http = require('http');
+const fs = require('fs')
 
 const server = http.createServer((req,res)=>{
+    let method = req.method
+    if(req.url === '/'){
+    res.write('<html>')
+    res.write('<body><form action="/message"><input type ="text" name="message"><button>Send</button></form></body>');
+    res.write('</html>')
+     return res.end();
+    }
+    if(req.url === '/home'){
+        res.write('<html>')
+        res.write('<body><h1>Welcome to Home Page</h1></body>');
+        res.write('</html>')
+         return res.end();
+    }
+    if(req.url === '/about'){
+        res.write('<html>')
+        res.write('<body><h1>Welcome to about us page</h1></body>');
+        res.write('</html>')
+         return res.end();
+    }
+    if(req.url === '/node'){
+        res.write('<html>')
+        res.write('<body><h1>Welcome to my node project</h1></body>');
+        res.write('</html>')
+         return res.end();
+    }
+    if(req.url === '/message' && method === 'POST'){
+       fs.writeFileSync('message.text','DUMMY');
+       res.statusCode = 302;
+       res.setHeader = ('Location','/')
+    }
     res.setHeader('Content-Type','text/html');
     res.write('<html>')
     res.write('<body><h1>My name is vignesh</h1></body>');
