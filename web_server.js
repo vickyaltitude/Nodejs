a38e4877a2e8283eedcde0b7256f7e3bb59e117b
vickyaltitude <vignvick3005>
2024-09-26 12:11:38 +0530

    used middleware with app.use method

--- web_server.js
+++ web_server.js
@@ -3,21 +3,32 @@ const app = express ();
 const fs = require('fs');
 const path = require('path');
 const PORT = process.env.PORT || 3400;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 /* app.get('^/$|/tipscalculator',(req,res)=>{
     res.sendFile(path.join(__dirname,'tipscalculator.html'))
 }) */
 
-app.use((req,res,next)=>{
-    
-    console.log('in the middleware')
-    next()
-})
 
-app.use((req,res,next)=>{
+    app.use('/product-page',(req,res,next)=>{
  
-    console.log('in another middleware');
-    res.send('<h1>Hello world from express</h1>')
-})
+        res.send('<body><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Get product</button></body>')
+    })
+
+    app.use('/product',(req,res,next)=>{
+        console.log(req.body)
+        res.send('<body>This is the product page</body>')
+    })
+
+    app.use('/',(req,res,next)=>{
+    
+        res.send('<h1>This is home</h1>')
+        
+    })
+
+
+
 
 app.listen(PORT,() => console.log(`server running on port ${PORT}`));
